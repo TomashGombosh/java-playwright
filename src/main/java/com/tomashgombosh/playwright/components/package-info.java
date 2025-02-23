c2b7a2ce33df21b1ac95cb762df9af99041ff444
Tomash Gombosh <blackjneco@gmail.com>
2025-02-23 16:16:41 +0200

    [TG-000]: Init the project

---
+++ src/main/java/com/tomashgombosh/playwright/components/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package contains classes that represent components of the application.
+ * <p>
+ *     Each component class should contain methods that represent actions that can be performed on the component.
+ *     For example, a navigation component class might contain a method to navigate to a specific page.
+ *     Component classes should not contain assertions, as those should be handled in the test classes or steps
+ *     Component classes should also not contain any Playwright code, as that should be handled in the test classes.
+ *     Component classes should be used to encapsulate the logic of the application, making it easier to maintain and update.
+ *     Component classes should be used to represent the different components of the application, making it easier to interact with them.
+ * </p>
+ */
+package com.tomashgombosh.playwright.components;
---
+++ src/main/java/com/tomashgombosh/playwright/services/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains services that are used to interact with the API, database, or other external services.
+ * <p>
+ * Each service class should contain methods that represent actions that can be performed on the service.
+ * For example, a user service class might contain a method to create a new user.
+ * Service classes should not contain assertions, as those should be handled in the test classes or steps
+ * Service classes should be used to encapsulate the logic of the service, making it easier to maintain and update.
+ * </p>
+ */
+
+package com.tomashgombosh.playwright.services;
